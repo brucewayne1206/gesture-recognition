c22881410708790ead30c855f9c824f31a8184c4
Arisnithin <hari42726@gmail.com>
2022-03-08 23:55:01 +0530

    Code cleanup (#2)

--- src/recongnition.js
+++ src/recongnition.js
@@ -1,39 +1,32 @@
-// 1. Add Use State
-// 2. Import emojis and finger pose import * as fp from "fingerpose";
-// 3. Setup hook and emoji object
-// 4. Update detect function for gesture handling
-// 5. Add emoji display to the screen
 
-///////// NEW STUFF ADDED USE STATE
 import React, { useRef, useState, useEffect } from "react";
-///////// NEW STUFF ADDED USE STATE
 
-// import logo from './logo.svg';
+
+
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import "./App.css";
 import { drawHand } from "./utils";
 
-///////// NEW STUFF IMPORTS
+
 import * as fp from "fingerpose";
 import victory from "./victory.png";
 import thumbs_up from "./thumbs_up.png";
-///////// NEW STUFF IMPORTS
+
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  ///////// NEW STUFF ADDED STATE HOOK
+
   const [emoji, setEmoji] = useState(null);
   const images = { thumbs_up: thumbs_up, victory: victory };
-  ///////// NEW STUFF ADDED STATE HOOK
+
 
   const runHandpose = async () => {
     const net = await handpose.load();
     console.log("Handpose model loaded.");
-    //  Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 10);
@@ -61,9 +54,6 @@ function App() {
 
       // Make Detections
       const hand = await net.estimateHands(video);
-      // console.log(hand);
-
-      ///////// NEW STUFF ADDED GESTURE HANDLING
 
       if (hand.length > 0) {
         const GE = new fp.GestureEstimator([
@@ -72,23 +62,16 @@ function App() {
         ]);
         const gesture = await GE.estimate(hand[0].landmarks, 4);
         if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
-          // console.log(gesture.gestures);
-
           const confidence = gesture.gestures.map(
             (prediction) => prediction.confidence
           );
           const maxConfidence = confidence.indexOf(
             Math.max.apply(null, confidence)
           );
-          // console.log(gesture.gestures[maxConfidence].name);
           setEmoji(gesture.gestures[maxConfidence].name);
-          console.log(emoji);
         }
       }
 
-      ///////// NEW STUFF ADDED GESTURE HANDLING
-
-      // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
       drawHand(hand, ctx);
     }
@@ -146,8 +129,6 @@ function App() {
         ) : (
           ""
         )}
-
-        {/* NEW STUFF */}
       </header>
     </div>
   );
